1dc8813b1531e25a62f8cfa4dcb41b2297825b86
kellyvigil <kellywvigil@gmail.com>
2014-05-19 23:06:01 -0700

    bouncy bll

--- JTT.js
+++ JTT.js
@@ -31,5 +31,27 @@ $(document).ready(function() {
 	$(function(){
     $('li').okhover();  
 }):
-	
+
 });
+
+function init()
+{
+  context= myCanvas.getContext('2d');
+  setInterval(draw,10);
+}
+
+function draw()
+{
+  context.clearRect(0,0, 300,300);
+  context.beginPath();
+  context.fillStyle="#0000ff";
+  // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
+  context.arc(x,y,20,0,Math.PI*2,true);
+  context.closePath();
+  context.fill();
+  // Boundary Logic
+if( x<0 || x>300) dx=-dx; 
+if( y<0 || y>300) dy=-dy; 
+x+=dx; 
+y+=dy;
+}
